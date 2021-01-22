cef783cc52499014b9c6de4d69cb9e4a6a56eaf3
jeing <53941317+jeingwi19@users.noreply.github.com>
2021-01-22 23:58:44 +0900

    210122 업로드

--- js/pj_theSoop.js
+++ js/pj_theSoop.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   //스크롤하면 가장 위에 배경이미지 opacity
   $(window).on('scroll', function(){
     var scrollY = $(this).scrollTop();
-    console.log(scrollY);
+    var docHei = $(document).height();
+    console.log(scrollY, docHei);
     if(scrollY > 100){
       $('.fixed_bg').addClass('on');
     }else{
@@ -24,9 +25,15 @@ $(document).ready(function(){
     }
 
     //.soopie3 캐릭터 서서히 나타나기
-    $('.soopie3').each(function (index) {
-      
-    });
+    //현재의 스크롤이 $(document)의 1/2지점 이상을 만족하면 addClass('on') 아니면 removeClass('on');
+      //1) 문서 전체 스크롤 높이를 구하기 = 6114
+      //2) 전체 높이에서 1/2해준다.
+      //?? fade? 해줘야하나? 애니메이션CSS 번복 오류
+    if(scrollY > docHei * 1/2){
+      $('.soopie3').addClass('on');
+    }else{
+      $('.soopie3').removeClass('on');
+    }
   });
 
   //#cnt1 .m_view .m_main a에 포커싱,마우스오버 했을때 애니메이션을 멈추기
