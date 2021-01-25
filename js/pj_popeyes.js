df54b7783da28f03cb4ffd42ce741b1a3f87276f
jeing <53941317+jeingwi19@users.noreply.github.com>
2021-01-25 23:16:00 +0900

    210125 업로드

--- js/pj_popeyes.js
+++ js/pj_popeyes.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+  /* 첫번째 PC화면 커서,포커싱하면 애니메이션 작동 */
+  //1)포커싱
+  $('.dev_link').on({
+    'focus mouseenter': function(){
+      $(this).children().children('.devi1').addClass('on');
+    },
+    'blur mouseleave': function(){
+      $(this).children().children('.devi1').removeClass('on');
+    }
+  });
+
   //푸터 Return to top 버튼 스크롤 맨위로
   $('.ft_txt2 a').on('click', function(){
     $('html, body').stop().animate({scrollTop: 0});
